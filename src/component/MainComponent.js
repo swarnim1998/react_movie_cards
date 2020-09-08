7aa78854d8b9b06bec5fcf339a09241c8c9dae09
Vaibhav Srivastava <vaibhavsriv67@gmail.com>
2020-09-08 12:14:41 +0530

    Creates MainComponent which will consist all the movie cards.

---
+++ src/component/MainComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MovieCard from '../MovieCard';
+
+function MainComponent(props) {
+  console.log(props);
+  const movieComponent = props.movieList
+    ? props.movieList.map((item) => <MovieCard key={item.id} data={item} />)
+    : null;
+  return (
+    <div>
+      <div className='container'>{movieComponent}</div>;
+    </div>
+  );
+}
+
+export default MainComponent;
