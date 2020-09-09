57de8e2cb0d70edef449c2f94aed9a7d9e3881ca
Vaibhav Srivastava <vaibhavsriv67@gmail.com>
2020-09-09 13:28:21 +0530

    Creates Home and About link to use for Route

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <nav>
       <h1>React Movie Cards</h1>
+      <Link to='/'>
+        <li>Home</li>
+      </Link>
+      <Link to='/about'>
+        <li>About</li>
+      </Link>
     </nav>
   );
 }
